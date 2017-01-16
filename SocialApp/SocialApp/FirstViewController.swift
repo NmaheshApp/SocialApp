91a2d6efa4b63f1fd6b076292f76b3b4386ef6a2
nithin mahesh <nithinmahesh@nithins-MacBook-Pro.local>
2017-01-16 17:13:02 -0500

    Adding starter code files

---
+++ SocialApp/SocialApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SocialApp
+//
+//  Created by nithin mahesh on 1/16/17.
+//  Copyright © 2017 nithin mahesh. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
